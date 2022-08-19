f9e428da7ea3971f084ba88782378a467a34e29a
Bree Hewlett <breevhew102@brees-mbp.mynetworksettings.com>
2022-08-18 21:26:13 -0700

    JS updated pt 2

--- index.js
+++ index.js
@@ -15,18 +15,39 @@ class XOSquare {
         this.button = document.getElementById(buttonId);
         if (this.button.innerText === "") {
             this.button.innerText = currentChr;
+            if (currentChr === "X"){
+                XPoint.push(this);
+            } else {
+                OPoint.push(this);
+            }
             switchChr();
+            checkWin();
         }
     }
     reset() {
         this.button.innerText = "";
     }
 }
+class winningPossibility {
+    constructor(x1, y1, x2, y2, x3, y3){
+        this.x1 = x1;
+        this.y1 = y1; 
+        this.x2 = x2;
+        this.y2 = y2;
+        this.x3 = x3;
+        this.y3 = y3;
+    }
+}
+function checkWinningPossibility(winningPossibility, forChr) {
+    let 
+}
 function switchChr() {
     if (currentChr === "X") {
         currentChr = "O";
+        statusLabel.innerText = "O's turn";
     } else {
         currentChr = "X";
+        statusLabel.innerText = "X's turn";
     }
 }
 function setup() {
