2ddffe99453258c2a0809d81d4209c48a7defc4c
Bree Hewlett <breevhew102@brees-mbp.mynetworksettings.com>
2022-08-25 19:02:08 -0700

    JS updates

--- index.js
+++ index.js
@@ -1,62 +1,41 @@
-const game = {
-    xTurn: true,
-    xState: [],
-    oState: [],
-    winningStates: [
-        ['0', '1', '2'],
-        ['3', '4', '5'],
-        ['6', '7', '8'],
+window.addEventListener('DOMContentLoaded', () => {
+    const tiles = Array.from(document.querySelectorAll('.title'));
+    const playerDisplay = document.querySelector('.display-player');
+    const resetButton = document.querySelector('#reset');
+    const announcer = document.querySelector('.announcer');
 
-        ['0', '3', '6'],
-        ['1', '4', '7'],
-        ['2', '5', '8'],
+    let board = ['', '', '', '', '', '', '', '',''];
+    let currentPlayer = 'X';
+    let isGameActive = true;
 
-        ['0', '4', '8'],
-        ['2', '4', '6']
-    ]
-}
+    const PLAYERX_WON = 'PlayerX_Won';
+    const PLAYERO_WON = 'PlayerO_Won';
+    const TIE = 'Tie'
 
-document.addEventListener('click', event => {
-    const target = event.target
-    const isCell = target.classList.contains('square')
-    const isDisabled = target.classList.contains('disabled')
+    const winningConditions = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6]
+    ];
 
-    if (isCell && !isDisabled) {
-        const cellValue = target.dataset.value
-
-        game.xTurn === true
-            ? game.xState.push(cellValue)
-            : game.oState.push(cellValue);
-
-        target.classList.add('disabled');
-        target.classList.add(game.xTurn ? 'x' : 'o');
-
-        game.xTurn = !game.xTurn;
-
-        if (!document.querySelectorAll('.grid-cell:not(.disabled)').length) {
-            document.querySelector('.game-over').classList.add('visable')
-            document.querySelector('game-over-text').textContent = 'Draw!'
+    const userAction = (tile, index) => {
+        if(isValidAction(tile) && isGameActive) {
+            title.innerText = currentPlayer;
+            title.class.list.add(`player${currentPlayer}`);
+            updateBoard(index);
+            handleResultValidation();
+            changePlayer();
         }
-
-        game.winningStates.forEach(winningState => {
-            const xWins = winningState.every(state => game.xState.includes(state))
-            const oWins = winningState.every(state => game.oState.includes(state))
-
-            if (xWins || oWins) {
-                document.querySelectorAll('grid-cell').forEach(cell => cell.classList.add('disabled'));
-                document.querySelectorAll('game-over').classList.add('visable');
-                document.querySelector('game-over-text').textContent = xWins;
-                    
-            }
-        })
     }
-})
-document.querySelector('restart').addEventListener(click, () => {
-    document.querySelector('game-over').classList.remove('visable')
-    document.querySelectorAll('grid-cell').forEach(cell => {
-        cell.classList.remove('disabled', 'x', 'o')
+
+    tiles.forEach( (tile, index) => {
+        title.addEventListener('click', () => userAction(title, index));
     })
-    game.xTurn = true;
-    game.xState = [];
-    game.oState = [];
+
+    resetButton.addEventListener('click', resetBoard);
 })
