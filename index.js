0e898de8c8815dd8a1849de4192df9a4ffd7829d
Bree Hewlett <breevhew102@brees-mbp.mynetworksettings.com>
2022-08-25 18:22:39 -0700

    Updated HTML

--- index.js
+++ index.js
@@ -15,3 +15,48 @@ const game = {
         ['2', '4', '6']
     ]
 }
+
+document.addEventListener('click', event => {
+    const target = event.target
+    const isCell = target.classList.contains('square')
+    const isDisabled = target.classList.contains('disabled')
+
+    if (isCell && !isDisabled) {
+        const cellValue = target.dataset.value
+
+        game.xTurn === true
+            ? game.xState.push(cellValue)
+            : game.oState.push(cellValue);
+
+        target.classList.add('disabled');
+        target.classList.add(game.xTurn ? 'x' : 'o');
+
+        game.xTurn = !game.xTurn;
+
+        if (!document.querySelectorAll('.grid-cell:not(.disabled)').length) {
+            document.querySelector('.game-over').classList.add('visable')
+            document.querySelector('game-over-text').textContent = 'Draw!'
+        }
+
+        game.winningStates.forEach(winningState => {
+            const xWins = winningState.every(state => game.xState.includes(state))
+            const oWins = winningState.every(state => game.oState.includes(state))
+
+            if (xWins || oWins) {
+                document.querySelectorAll('grid-cell').forEach(cell => cell.classList.add('disabled'));
+                document.querySelectorAll('game-over').classList.add('visable');
+                document.querySelector('game-over-text').textContent = xWins;
+                    
+            }
+        })
+    }
+})
+document.querySelector('restart').addEventListener(click, () => {
+    document.querySelector('game-over').classList.remove('visable')
+    document.querySelectorAll('grid-cell').forEach(cell => {
+        cell.classList.remove('disabled', 'x', 'o')
+    })
+    game.xTurn = true;
+    game.xState = [];
+    game.oState = [];
+})
